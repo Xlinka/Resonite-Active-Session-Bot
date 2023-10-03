e0736954775c328ec13a8f3b128fff11aede3cca
xLinka <Oliver.dunnington@outlook.com>
2023-10-03 16:04:15 +0100

    Update SendSessionUpdates.js

--- Functions/SendSessionUpdates.js
+++ Functions/SendSessionUpdates.js
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch');
-const { EmbedBuilder } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const config = require('../config.json');
 const channelId = config.CHANNEL_ID;
-
 const apiUrl = config.API_URL;
 
 function removeHtmlColorTags(text) {
@@ -29,15 +28,15 @@ async function sendSessionUpdates(client) {
       throw new Error(`Received status ${response.status} ${response.statusText}`);
     }
     const sessions = await response.json();
-    const activeSessions = sessions.filter(session => !session.hasEnded && session.activeUsers > 0);
+    const activeSessions = sessions.filter(session => session.isValid && !session.hasEnded);
 
-    console.log(`Fetched ${activeSessions.length} active sessions from Neos API`);
+    console.log(`Fetched ${activeSessions.length} active sessions from API`);
 
     const channel = await client.channels.fetch(channelId);
 
     // Fetch previous session messages in the channel
     const messages = await channel.messages.fetch();
-    const sessionMessages = messages.filter(message => message.author.id === client.user.id && message.embeds.length > 0 && message.embeds[0].footer?.text === 'Session information updated');
+    const sessionMessages = messages.filter(message => message.author.id === client.user.id && message.embeds.length > 0);
     const sessionMessageIds = sessionMessages.map(message => message.id);
 
     // Delete previous session messages in the channel
@@ -46,62 +45,33 @@ async function sendSessionUpdates(client) {
     }
 
     // Send updated session messages
-    const newSessionMessageIds = [];
     for (const [index, session] of activeSessions.entries()) {
-      let imageUrl = session.thumbnail;
-      if (imageUrl && imageUrl.startsWith('neosdb:///')) {
-        imageUrl = imageUrl.replace('neosdb:///', 'https://assets.neos.com/assets/').replace('.webp', '');
-      }
       const cleanName = removeHtmlColorTags(session.name);
       const uptime = calculateUptime(session.sessionBeginTime);
 
-      let sessionUrls = session.sessionURLs
-      .filter(url => url != null)
-      .map(url => {
-        if (url.startsWith('lnl-nat://')) {
-          return `\`${url}\``;
-        } else if (url.startsWith('neos-steam://')) {
-          return `\`${url}\``;
-        } else {
-          return '';
-        }
-      })
-      .join('\n');
-
-      const userList = session.sessionUsers.map(user => user.username).join(', ');
-
-      const embed = new EmbedBuilder()
+      const embed = new MessageEmbed()
         .setTitle(`Session ${index + 1}: ${cleanName}`)
         .setColor('#0099ff')
-        .setDescription(session.description || 'No description provided')
         .addFields(
           { name: 'Host', value: session.hostUsername, inline: true },
-          { name: 'Users', value: `${session.activeUsers} (${session.totalActiveUsers})`, inline: true },
+          { name: 'Active Users', value: session.activeUsers.toString(), inline: true },
+          { name: 'Total Joined Users', value: session.totalJoinedUsers.toString(), inline: true },
           { name: 'Uptime', value: uptime, inline: true },
-          { name: 'Neos Version', value: session.neosVersion, inline: true },
-          { name: 'Headless Server', value: session.headlessHost.toString(), inline: true },
-          { name: 'Mobile Friendly', value: session.mobileFriendly.toString(), inline: true },
-          { name: 'Session URLs', value: sessionUrls },
-          { name: 'Join Session', value: `http://cloudx.azurewebsites.net/open/session/${session.sessionId}` },
-          { name: 'Users', value: userList },
+          { name: 'App Version', value: session.appVersion, inline: true },
+          { name: 'Headless Host', value: session.headlessHost.toString(), inline: true },
+          { name: 'Mobile Friendly', value: session.mobileFriendly.toString(), inline: true }
         )
-        .setImage(imageUrl)
+        .setImage(session.thumbnailUrl)
         .setTimestamp()
-        .setFooter({ text: 'Session information updated' });
+        .setFooter('Session information updated');
 
-      const message = await channel.send({ embeds: [embed] });
-      newSessionMessageIds.push(message.id);
+      await channel.send({ embeds: [embed] });
     }
-
-
-    // Store new session message IDs for future updates
-    sessionMessageIds.push(...newSessionMessageIds);
 
   } catch (error) {
     console.error(`Error while fetching or sending session updates: ${error.message}`);
     console.error(error.stack);
   }
 }
-
 
 module.exports = sendSessionUpdates;
